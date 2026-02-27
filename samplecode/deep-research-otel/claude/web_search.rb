aeef59ee00bc90dd8fd165709f936def9ff610d4
ftnext <takuyafjp+develop@gmail.com>
2026-02-27 22:30:27 +0900

    feat: Anthropic search example

---
+++ samplecode/deep-research-otel/claude/web_search.rb
@@ -0,0 +1,36 @@
+# https://github.com/anthropics/anthropic-sdk-ruby/blob/main/examples/web_search.rb
+require "anthropic"
+
+anthropic = Anthropic::Client.new
+
+message = anthropic.messages.create(
+  model: "claude-haiku-4-5-20251001",
+  max_tokens: 1024,
+  messages: [{role: :user, content: "What's the weather in New York?"}],
+  tools: [
+    {
+      name: "web_search",
+      type: "web_search_20250305",
+    }
+  ]
+)
+
+message
+  .content
+  .each do |content|
+    case content
+    when Anthropic::ServerToolUseBlock
+      pp("Tool use: ---")
+      pp(content.input)
+    when Anthropic::WebSearchToolResultBlock
+      pp("Search: ---")
+      pp(content.content)
+    when Anthropic::TextBlock
+      pp("Text: ---")
+      pp(content.text)
+    end
+  end
+
+pp("Input tokens: #{message.usage.input_tokens}")
+
+pp("Output tokens: #{message.usage.output_tokens}")
